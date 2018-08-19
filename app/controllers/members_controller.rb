b5db12039b0bfdeabc510ca4b1f450d20f5a94c4
Piotr Sampławski <sampi60@gmail.com>
2018-08-19 20:02:02 +0200

    Paginacja

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,7 +5,7 @@ class MembersController < ApplicationController
   # GET /members
   # GET /members.json
   def index
-    @members = Member.all
+    @members = Member.all.paginate(page: params[:page])
   end
 
   # GET /members/1
